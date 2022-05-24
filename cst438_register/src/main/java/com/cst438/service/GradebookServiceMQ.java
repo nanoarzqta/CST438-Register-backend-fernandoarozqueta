e8ae400e5c92e7f6940c01d1397eb0e34e21c758
nanoarzqta <farozqueta@csumb.edu>
2022-05-24 13:09:40 -0700

    Assignment 4 update
    Part 1.  Develop and test synchronous REST communication between services.
    Part 2.  Develop and test Asynchronous Message communication

--- cst438_register/src/main/java/com/cst438/service/GradebookServiceMQ.java
+++ cst438_register/src/main/java/com/cst438/service/GradebookServiceMQ.java
@@ -5,9 +5,12 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;										   
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;															  
 
 import com.cst438.domain.CourseDTOG;
+import com.cst438.domain.CourseDTOG.GradeDTO;											 
 import com.cst438.domain.Enrollment;
 import com.cst438.domain.EnrollmentDTO;
 import com.cst438.domain.EnrollmentRepository;
@@ -34,7 +37,12 @@ public class GradebookServiceMQ extends GradebookService {
 	public void enrollStudent(String student_email, String student_name, int course_id) {
 		 
 		//TODO  complete this method in homework 4
+		//Create new EnrollmentDTO object
+		EnrollmentDTO newStudent = new EnrollmentDTO(student_email, student_name, course_id);
 		
+		//TODO  complete this method in homework 4
+		this.rabbitTemplate.convertAndSend(gradebookQueue.getName(), newStudent);
+		System.out.println(" [x] Sent '" + newStudent + "'");																			   													   
 	}
 	
 	@RabbitListener(queues = "registration-queue")
@@ -43,8 +51,29 @@ public class GradebookServiceMQ extends GradebookService {
 		
 		//TODO  complete this method in homework 4
 		
-	}
-	
-	
+		for (CourseDTOG.GradeDTO grade : courseDTOG.grades) { //test
+		//	System.out.println("Name: " + grade.student_name + " Email: " 
+		//			+ grade.student_email + " Grade: " + grade.grade);
 
+			String finalGrade = grade.grade;
+			int courseID = courseDTOG.course_id;
+			String studentEmail = grade.student_email;
+			String studentName = grade.student_name;
+			
+			System.out.println("Name: " + studentName + " email: " + 
+					studentEmail + " course ID: " + courseID + 
+					" final grade: " + finalGrade);
+			
+			//Get enrollment for Student
+			Enrollment enrollment = enrollmentRepository.findByEmailAndCourseId(studentEmail, courseID);
+
+			if (enrollment != null) {
+				//Set grade for enrollment
+				enrollment.setCourseGrade(finalGrade);
+				//Write enrollment
+				enrollmentRepository.save(enrollment);
+			} else {
+				throw  new ResponseStatusException( HttpStatus.BAD_REQUEST, "Could not find record for enrollment.");
+			}			
+		}											   
 }
--- cst438_register/src/main/java/com/cst438/service/GradebookServiceREST.java
+++ cst438_register/src/main/java/com/cst438/service/GradebookServiceREST.java
@@ -1,6 +1,8 @@
 package com.cst438.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;										   									   
 import org.springframework.web.client.RestTemplate;
 
 import com.cst438.domain.EnrollmentDTO;
@@ -20,8 +22,25 @@ public class GradebookServiceREST extends GradebookService {
 	@Override
 	public void enrollStudent(String student_email, String student_name, int course_id) {
 		
-		//TODO  complete this method in homework 4
+		/this class will send a new student enrollment to the GradeBook 
+		//service using HTTP POST and an EnrollmentDTO object.
 		
+		//Create new EnrollmentDTO object
+		EnrollmentDTO newStudent = new EnrollmentDTO(student_email, student_name, course_id);
+
+		System.out.println("Gradebook URL: "+gradebook_url + "/enrollment");
+		//Send to gradebook_url
+		System.out.println("Sending http message: "+newStudent);
+		ResponseEntity<EnrollmentDTO> response = restTemplate.postForEntity(
+				gradebook_url + "/enrollment",   		// URL
+				newStudent,				// data to send
+				EnrollmentDTO.class);   // return data type
+		
+		HttpStatus rc = response.getStatusCode();
+		System.out.println("HttpStatus: "+rc);
+		EnrollmentDTO returnObject = response.getBody();
+		System.out.println(returnObject);
+		//return returnObject;									
 	}
 
 }
